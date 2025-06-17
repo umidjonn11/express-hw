57092663d03c9795cff1bc0a0937a3410ff73c48
Umedov Umidjon <lustrum062@gmail.com>
2025-06-17 19:55:44 +0300

    fix

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,33 +1,27 @@
-const request = require('supertest');
-const app = require("../index.js"); 
+const request = require("supertest");
+const app = require("../app");
 
-describe('GET /hello', () => {
-  it('should return Hello, World!', async () => {
-    const res = await request(app).get('/hello');
+describe("GET /hello", () => {
+  it("should return Hello, World!", async () => {
+    const res = await request(app).get("/hello");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello, World!');
+    expect(res.text).toBe("Hello, World!");
   });
 });
 
-describe('POST /sum', () => {
-  it('should return the correct sum', async () => {
-    const res = await request(app)
-      .post('/sum')
-      .send({ a: 3, b: 4 });
+describe("POST /sum", () => {
+  it("should return the correct sum", async () => {
+    const res = await request(app).post("/sum").send({ a: 3, b: 4 });
     expect(res.statusCode).toBe(200);
     expect(res.body.result).toBe(7);
   });
 
-  it('should return 400 for invalid input', async () => {
-    const res = await request(app)
-      .post('/sum')
-      .send({ a: 'foo', b: 2 });
+  it("should return 400 for invalid input", async () => {
+    const res = await request(app).post("/sum").send({ a: "foo", b: 2 });
     expect(res.statusCode).toBe(400);
-    expect(res.body).toHaveProperty('error');
+    expect(res.body).toHaveProperty("error");
   });
 });
-
-
 
 // test("should return hello world", () => {
 //   fetch("http://localhost:3030/hello")
